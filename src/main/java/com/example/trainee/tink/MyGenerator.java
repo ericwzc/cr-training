1f91cb20a9fbd4caacad8763184890ef0195bd9d
Tink Cai <Tink.Cai@dbschenker.com>
2021-10-27 11:28:33 +0800

    MyGenerator of Tink Cai

--- src/main/java/com/example/trainee/tink/MyGenerator.java
+++ src/main/java/com/example/trainee/tink/MyGenerator.java
@@ -3,13 +3,14 @@ package com.example.trainee.tink;
 import com.example.demo.id.IdGenerator;
 
 import java.text.NumberFormat;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyGenerator implements IdGenerator {
-    private Map<String, Integer> numMap;
+    private Map<String, AtomicInteger> numMap;
     MyGenerator() {
-        numMap = new HashMap<>();
+        numMap = new ConcurrentHashMap<String, AtomicInteger>();
     }
 
     private String transformToString(int number, int maxLength) {
@@ -19,14 +20,19 @@ public class MyGenerator implements IdGenerator {
         return formatter.format(number);
     }
 
-    private Integer getNumberByKey(String key) {
-        return numMap.getOrDefault(key, 0);
+    private AtomicInteger getNumberByKey(String key) {
+        numMap.putIfAbsent(key, new AtomicInteger(0));
+        return numMap.get(key);
+    }
+
+    private String getTimestamp() {
+        return "";
     }
 
     @Override
     public String getId(String prefix) {
-        Integer currentNumber = getNumberByKey(prefix);
-        numMap.put(prefix, ++currentNumber);
+        AtomicInteger currentNumberObject = getNumberByKey(prefix);
+        int currentNumber = currentNumberObject.incrementAndGet();
         return prefix + transformToString(currentNumber, 10);
     }
 }
