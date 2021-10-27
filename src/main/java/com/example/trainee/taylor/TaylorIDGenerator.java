411f4d2ccc9675137b88e63557d073c6b3ed07c1
Taylor Chen <taylor.chen@dbschenker.com>
2021-10-27 15:01:06 +0800

    multiple threads for ID Gen

--- src/main/java/com/example/trainee/taylor/TaylorIDGenerator.java
+++ src/main/java/com/example/trainee/taylor/TaylorIDGenerator.java
@@ -3,27 +3,20 @@ package com.example.trainee.taylor;
 import com.example.demo.id.IdGenerator;
 
 import java.util.*;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaylorIDGenerator implements IdGenerator {
-    private HashMap<String, Integer> idMap = new HashMap<>();
-    private int suffixNum = 1;
-    private Semaphore mutex = new Semaphore(1);
+    private ConcurrentHashMap<String, AtomicInteger> idMap = new ConcurrentHashMap<>();
 
     @Override
     public String getId(String prefix) {
-        try {
-            mutex.acquire();
-            if (idMap.containsKey(prefix)) {
-                suffixNum = idMap.get(prefix) + 1;
-            }
-            idMap.put(prefix, suffixNum);
-            return prefix + ":" + suffixNum;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } finally {
-            mutex.release();
+        if(idMap.containsKey(prefix)){
+            idMap.computeIfPresent(prefix, (key , val)  -> idMap.put(key, new AtomicInteger(val.getAndIncrement())));
+        } else {
+            idMap.computeIfAbsent(prefix, k-> new AtomicInteger(1));
         }
-        return "bad sequence!";
+
+        return Thread.currentThread().getName() + ":" + prefix+":"+ idMap.get(prefix);
     }
 }
