85e3df8ebefd306daeb9733422ec491071cf6379
Taylor Chen <taylor.chen@dbschenker.com>
2021-10-27 10:57:15 +0800

    multiple threads

--- src/main/java/com/example/trainee/taylor/TaylorIDGenerator.java
+++ src/main/java/com/example/trainee/taylor/TaylorIDGenerator.java
@@ -3,16 +3,27 @@ package com.example.trainee.taylor;
 import com.example.demo.id.IdGenerator;
 
 import java.util.*;
+import java.util.concurrent.Semaphore;
 
 public class TaylorIDGenerator implements IdGenerator {
     private HashMap<String, Integer> idMap = new HashMap<>();
     private int suffixNum = 1;
+    private Semaphore mutex = new Semaphore(1);
+
     @Override
     public String getId(String prefix) {
-        if(idMap.containsKey(prefix)) {
-            suffixNum = idMap.get(prefix) + 1;
+        try {
+            mutex.acquire();
+            if (idMap.containsKey(prefix)) {
+                suffixNum = idMap.get(prefix) + 1;
+            }
+            idMap.put(prefix, suffixNum);
+            return prefix + ":" + suffixNum;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            mutex.release();
         }
-        idMap.put(prefix,suffixNum);
-        return prefix+":"+ suffixNum;
+        return "bad sequence!";
     }
 }
