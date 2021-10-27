ddd327c6b6b71f6a79c5dd3f96b47423aafacc35
cathy <cathy-hy.wang@dbschenker.com>
2021-10-27 10:15:59 +0800

    add cathy package

--- src/main/java/com/example/trainee/cathy/IdGeneratorImpl.java
+++ src/main/java/com/example/trainee/cathy/IdGeneratorImpl.java
@@ -2,19 +2,39 @@ package com.example.trainee.cathy;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.example.demo.id.IdGenerator;
 
 public class IdGeneratorImpl implements IdGenerator {
 
-	private int runnningNumber = 0;
+	private Map<String, Integer> cacheMap = new HashMap<String, Integer>();
 	
 	@Override
 	public String getId(String prefix) {
-		return prefix+ now() + String.format("%05d", ++runnningNumber);
+		return prefix + "-" + now() + "-" + this.getRunningNumber(prefix);
 	}
 
 	private String now() {
 		return new SimpleDateFormat("yyyyMMddHHmm").format(Calendar.getInstance().getTime());
+	}
+	
+	private String getRunningNumber(String prefix) {
+		Integer value = cacheMap.get(prefix);
+		
+		//set init value 
+		if(value == null) {
+			value = 0;
+			cacheMap.put(prefix, value);
+		}
+		
+		//increment by 1 
+		++value;
+		
+		//reset new value
+		cacheMap.put(prefix, value);
+		
+		return String.format("%05d", value);
 	}
 }
