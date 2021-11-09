2247ed06ba76e2545debd45e092064fd998d1285
Taylor Chen <taylor.chen@dbschenker.com>
2021-11-09 18:39:20 +0800

    Replace AtomicIntegerArray with AtomicLongArray to fix timestamp issue

--- src/main/java/com/example/trainee/taylor/TaylorIDGenerator.java
+++ src/main/java/com/example/trainee/taylor/TaylorIDGenerator.java
@@ -6,19 +6,16 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.concurrent.atomic.AtomicLongArray;
 
 public class TaylorIDGenerator implements IdGenerator {
-    private ConcurrentHashMap<String, AtomicIntegerArray> idMap;
-    private int lastMinute;
+    private ConcurrentHashMap<String, AtomicLongArray> idMap;
 
     private static final int LAST_MINUTE_INDEX = 0;
     private static final int CURRENT_SEQ_INDEX = 1;
 
     public TaylorIDGenerator(){
         idMap = new ConcurrentHashMap<>();
-        lastMinute = Calendar.getInstance().get(Calendar.MINUTE);
     }
     @Override
     public String getId(String prefix) {
@@ -26,25 +23,25 @@ public class TaylorIDGenerator implements IdGenerator {
     }
 
     private String sequenceGen(String prefix) {
-        idMap.putIfAbsent(prefix, new AtomicIntegerArray(new int[]{lastMinute, 0}));
-        AtomicIntegerArray atomicIntArray = idMap.get(prefix);
+        idMap.putIfAbsent(prefix, new AtomicLongArray(new long[]{dateFormat(Calendar.getInstance().getTime()), 0}));
+        AtomicLongArray atomicArray = idMap.get(prefix);
 
         //Reset sequence based on every minute
         Calendar calendar = Calendar.getInstance();
-        int currentMinute =  calendar.get(Calendar.MINUTE);
-        int lastMinute = atomicIntArray.get(LAST_MINUTE_INDEX);
-        if(currentMinute > lastMinute || (currentMinute == 0 && lastMinute == 59)) {
-            atomicIntArray.set(LAST_MINUTE_INDEX, currentMinute);
-            atomicIntArray.set(CURRENT_SEQ_INDEX, 0);
+        long currentMinute =  dateFormat(calendar.getTime());
+        long lastMinute = atomicArray.get(LAST_MINUTE_INDEX);
+        if(currentMinute > lastMinute) {
+            atomicArray.set(LAST_MINUTE_INDEX, currentMinute);
+            atomicArray.set(CURRENT_SEQ_INDEX, 0);
         }
 
-        String currentTimeStamp = dateFormat(calendar.getTime());
-        int currentSeq = atomicIntArray.incrementAndGet(CURRENT_SEQ_INDEX);
+        long currentTimeStamp = dateFormat(calendar.getTime());
+        long currentSeq = atomicArray.incrementAndGet(CURRENT_SEQ_INDEX);
         return String.format("%s-%s-%s",prefix,currentTimeStamp,currentSeq);
     }
 
-    private String dateFormat(Date date){
+    private long dateFormat(Date date) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
-        return formatter.format(date);
+        return Long.parseLong(formatter.format(date));
     }
 }
