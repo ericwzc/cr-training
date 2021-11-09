f8c0cc23122f05bdb0b05ce03f9189ec257f23bc
Taylor Chen <taylor.chen@dbschenker.com>
2021-11-09 17:36:44 +0800

    add reset function based on every minute

--- src/main/java/com/example/trainee/taylor/TaylorIDGenerator.java
+++ src/main/java/com/example/trainee/taylor/TaylorIDGenerator.java
@@ -2,20 +2,43 @@ package com.example.trainee.taylor;
 
 import com.example.demo.id.IdGenerator;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaylorIDGenerator implements IdGenerator {
-    private ConcurrentHashMap<String, AtomicInteger> idMap = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, AtomicInteger> idMap;
+    private int lastMinute;
 
+    public TaylorIDGenerator(){
+        idMap = new ConcurrentHashMap<>();
+        lastMinute = Calendar.getInstance().get(Calendar.MINUTE);
+    }
     @Override
     public String getId(String prefix) {
-        if(idMap.containsKey(prefix)){
-            idMap.computeIfPresent(prefix, (key , val)  -> idMap.put(key, new AtomicInteger(val.getAndIncrement())));
-        } else {
-            idMap.computeIfAbsent(prefix, k-> new AtomicInteger(1));
+        return sequenceGen(prefix);
+    }
+
+    private String sequenceGen(String prefix) {
+        idMap.putIfAbsent(prefix, new AtomicInteger(0));
+        AtomicInteger currentVal = idMap.get(prefix);
+        Calendar calendar = Calendar.getInstance();
+        int currentMinute =  calendar.get(Calendar.MINUTE);
+        String currentTimeStamp = dateFormat(calendar.getTime());
+
+        //Reset Init Sequence
+        if(currentMinute != lastMinute) {
+            currentVal.set(0);
+            lastMinute = currentMinute;
         }
 
-        return prefix+":"+ idMap.get(prefix);
+        return String.format("%s-%s-%s",prefix,currentTimeStamp,currentVal.incrementAndGet());
+    }
+
+    private String dateFormat(Date date){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmm");
+        return formatter.format(date);
     }
 }
