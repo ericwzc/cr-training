802fb13ed6d7f1f3b4a93fc4f35c42277ac67dc7
bob <bob.zhou@dbschenker.com>
2021-11-10 09:55:08 +0800

    add time

--- src/main/java/com/example/trainee/bob/BobIdGenerator.java
+++ src/main/java/com/example/trainee/bob/BobIdGenerator.java
@@ -2,6 +2,7 @@ package com.example.trainee.bob;
 
 import com.example.demo.id.IdGenerator;
 
+import java.time.Instant;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -9,16 +10,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class BobIdGenerator implements IdGenerator {
     private Map<String, AtomicInteger> integerHashMap = new ConcurrentHashMap<>();
 
-    @Override
-    public String getId(String prefix) {
+    public String generateId(String prefix) {
         AtomicInteger value = integerHashMap.getOrDefault(prefix,new AtomicInteger(0));
         Integer result = value.getAndAdd(1);
         integerHashMap.putIfAbsent(prefix,value);
         return prefix+result;
     }
 
-
-
-
-
+    @Override
+    public String getId(String prefix) {
+        return generateId(prefix + Instant.now().toString());
+    }
 }
